3a58f81b31e4a12297db406934a09947fb1ccf24
Noel Fran Thomas <54130980+noelfranthomas@users.noreply.github.com>
2023-04-28 00:46:50 +0000

    DEV moded autodiff

--- autodiff/src/main.rs
+++ autodiff/src/main.rs
@@ -1,110 +1,221 @@
-use std::ops::{Add, Sub, Mul, Div};
+// use std::collections::HashMap;
+// use std::ops::{Add, Sub, Mul, Div};
 
-struct Node<T, U> {
-    data: T,
-    grad: U, // Should this always be f64?
-    
-    // graph construction
-    _backward: String, // ??
-    _prev: u64,// hashmap? hashset? vector?
-    _op: String
-}
-
-impl<T, U> Node<T, U>{
-    fn new(data: T, grad: U) -> Self {
-        Self {
-            data,
-            grad,
-            _backward: String::from(""),
-            _prev: 1,// hashmap? hashset?
-            _op: String::from(""),
-        }
-    }
-}
-
-// Add
-impl<T: Add<Output = T>, U> Add for Node<T, U> {
-    type Output = Self;
-
-    fn add(self, other: Self) -> Self::Output {
-        Self {
-            data: self.data + other.data,
-            grad: self.grad, // Can be propogated as result's grad
-
-            _backward: String::from(""),
-            _prev: 1,// hashmap? hashset?
-            _op: String::from("+"),
-        }
-    }
-}
-
-// Sub
-impl<T: Sub<Output = T>, U> Sub for Node<T, U> {
-    type Output = Self;
-
-    fn sub(self, other: Self) -> Self::Output {
-        Self {
-            data: self.data - other.data,
-            grad: self.grad, // Can be propogated as result's grad
-
-            _backward: String::from(""),
-            _prev: 1,// hashmap? hashset?
-            _op: String::from("+"),
-        }
-    }
-}
-
-// Mul
-impl<T: Mul<Output = T>, U> Mul for Node<T, U> {
-    type Output = Self;
-
-    fn mul(self, other: Self) -> Self::Output {
-        Self {
-            data: self.data * other.data,
-            grad: self.grad,
-
-            _backward: String::from(""),
-            _prev: 1,// hashmap? hashset?
-            _op: String::from("+"),
-        }
-    }
-}
-
-// Div
-impl<T: Div<Output = T>, U> Div for Node<T, U> {
-    type Output = Self;
-
-    fn div(self, other: Self) -> Self::Output {
-        Self {
-            data: self.data / other.data,
-            grad: self.grad, // Can be propogated as result's grad
-
-            _backward: String::from(""),
-            _prev: 1,// hashmap? hashset?
-            _op: String::from("+"),
-        }
-    }
-}
-
-// Should this be a trait/impl?
-fn print_type<T>(_: &T) {
-    println!("{}", std::any::type_name::<T>())
-}
-
-// Trace the graph
-// fn trace(root: &Value) {
-//     // Find all nodes
+// #[derive(Debug, Clone, Copy)]
+// enum Op {
+//     Add,
+//     Sub,
+//     Mul,
+//     Div,
 // }
 
+// struct Node<T, U> {
+//     data: T,
+//     grad: U,
+//     op: Op,
+//     backward: String,
+//     prev: HashMap<usize, Op>,
+// }
+
+// impl<T, U> Node<T, U>
+// where
+//     T: Add<Output = T> + Sub<Output = T> + Mul<Output = T> + Div<Output = T> + Copy,
+//     U: Copy,
+// {
+//     fn new(data: T, grad: U) -> Self {
+//         Self {
+//             data,
+//             grad,
+//             op: Op::Add,
+//             backward: String::from(""),
+//             prev: HashMap::new(),
+//         }
+//     }
+// }
+
+// // Add
+// impl<T, U> Add for Node<T, U>
+// where
+//     T: Add<Output = T> + Copy,
+//     U: Copy,
+// {
+//     type Output = Self;
+
+//     fn add(self, other: Self) -> Self::Output {
+//         Self {
+//             data: self.data + other.data,
+//             grad: self.grad,
+//             op: Op::Add,
+//             backward: String::from(""),
+//             prev: {
+//                 let mut map = HashMap::new();
+//                 map.insert(0, self.op);
+//                 map.insert(1, other.op);
+//                 map
+//             },
+//         }
+//     }
+// }
+
+// // Sub
+// impl<T, U> Sub for Node<T, U>
+// where
+//     T: Sub<Output = T> + Copy,
+//     U: Copy,
+// {
+//     type Output = Self;
+
+//     fn sub(self, other: Self) -> Self::Output {
+//         Self {
+//             data: self.data - other.data,
+//             grad: self.grad,
+//             op: Op::Sub,
+//             backward: String::from(""),
+//             prev: {
+//                 let mut map = HashMap::new();
+//                 map.insert(0, self.op);
+//                 map.insert(1, other.op);
+//                 map
+//             },
+//         }
+//     }
+// }
+
+// // Mul
+// impl<T, U> Mul for Node<T, U>
+// where
+//     T: Mul<Output = T> + Copy,
+//     U: Copy,
+// {
+//     type Output = Self;
+
+//     fn mul(self, other: Self) -> Self::Output {
+//         Self {
+//             data: self.data * other.data,
+//             grad: self.grad,
+//             op: Op::Mul,
+//             backward: String::from(""),
+//             prev: {
+//                 let mut map = HashMap::new();
+//                 map.insert(0, self.op);
+//                 map.insert(1, other.op);
+//                 map
+//             },
+//         }
+//     }
+// }
+
+// // Div
+// impl<T, U> Div for Node<T, U>
+// where
+//     T: Div<Output = T> + Copy,
+//     U: Copy,
+// {
+//     type Output = Self;
+
+//     fn div(self, other: Self) -> Self::Output {
+//         Self {
+//             data: self.data / other.data,
+//             grad: self.grad,
+//             op: Op::Div,
+//             backward: String::from(""),
+//             prev: {
+//                 let mut map = HashMap::new();
+//                 map.insert(0, self.op);
+//                 map.insert(1, other.op);
+//                 map
+//             },
+//         }
+//     }
+// }
+
+// use std::fmt;
+
+// impl fmt::Display for Op {
+//     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+//         match self {
+//             Op::Add => write!(f, "+"),
+//             Op::Sub => write!(f, "-"),
+//             Op::Mul => write!(f, "*"),
+//             Op::Div => write!(f, "/"),
+//         }
+//     }
+// }
+
+// fn main() {
+
+//     let x = Node::new(1.0, 0.0);
+//     let y = Node::new(2.0, 1.0);
+//     let z = Node::new(2.0, 1.0);
+
+//     let a = x + y;
+
+//     a = a * z;
+
+//     let b = a + x;
+
+    
+//     println!("data: {}, grad: {}, backward: {}, prev: {}", a.data, a.grad, a.backward, a.prev[&0]);
+// }
+
+
+
+
+
+
+use std::io;
+
 fn main() {
+    let a = [1, 2, 3, 4, 5];
 
-    let x = Node::new(1.0, 0.0);
-    let y = Node::new(2.0, 1.0);
+    println!("Please enter an array index.");
 
-    let a: Node<f64, f64>;
+    let mut index = String::new();
 
-    a = x + y;
+    io::stdin()
+        .read_line(&mut index)
+        .expect("Failed to read line");
 
-    println!("data: {}, grad: {}", a.data, a.grad);
-    print_type(&a);
+    let index: usize = index
+        .trim()
+        .parse()
+        .expect("Index entered was not a number");
+
+    let element = a[index];
+
+    println!("The value of the element at index {index} is: {element}");
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
