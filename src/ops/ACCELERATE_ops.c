8842258957b0454d170c7bbf82e8cea57464adf4
Noel Fran Thomas <54130980+noelfranthomas@users.noreply.github.com>
2023-10-23 11:25:32 -0600

    simplify

--- src/ops/ACCELERATE_ops.c
+++ src/ops/ACCELERATE_ops.c
@@ -5,16 +5,7 @@
 
 int ACCELERATE_config(Mesh *mesh_ptr){
 
-    char *device_type = (char*) malloc(1024*sizeof(char));
-    char *api_id = (char*) malloc(1024*sizeof(char));
-
-    free(device_type);
-
     Device device = {"ACCEL", "SoC", 0};
-
-    size_t a = 1024;
-
-    show_cpu_info(device_type, a);
 
     add_device(mesh_ptr->device_list, device);
 
