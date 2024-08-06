ce5e46a8a1879a4a0d6864cbcc7fa03b12302185
Noel Fran Thomas <noelfranthomas@gmail.com>
2024-08-06 01:09:55 -0600

    made the test a bit more fun

--- _daisy/tests/torch.cc
+++ _daisy/tests/torch.cc
@@ -2,7 +2,13 @@
 #include <iostream>
 
 int main() {
-  torch::Tensor tensor = torch::rand({2, 3});
-  std::cout << tensor << std::endl;
+  std::cout << "TESTING TORCH" << std::endl;
+  std::cout << "Torch version: " << TORCH_VERSION << std::endl;
+  torch::Tensor tensorA = torch::eye(3);
+  torch::Tensor tensorB = torch::randint(0, 10, {3, 3});
+  std::cout << "Tensor A: " << std::endl << tensorA << std::endl;
+  std::cout << "Tensor B: " << std::endl << tensorB << std::endl;
+  std::cout << "Tensor A + B: " << std::endl << tensorA + tensorB << std::endl;
+
   return 0;
 }
